e6b1e164a5ff796dc19b784d78d90814aeb097d9
larmop <lmonpla@teleco.upv.es>
2023-12-27 13:39:02 +0100

    version_final_fea

--- PCAClassifier.R
+++ PCAClassifier.R
@@ -7,7 +7,7 @@ PCA_Classifier <- function(name){
   # predicted_class - classification of the image (number ID)
   
   # Here, the optimum hyperparameters are used
-  threshold = 3954.6 
+  threshold = 2800
   metric = 'euclidean'
   friends = 3
   
@@ -30,29 +30,21 @@ PCA_Classifier <- function(name){
   blue <- as.vector(data_image[, , 3])
   test_image <- c(red, green, blue)
   
+  print(length(test_image))
+  print(length(means_pca))
+  
+  #Centering the data
+  #centered_test <- scale(test_image, center = means_pca, scale = F)
+  centered_test <- test_image - means_pca
+  
   # Apply the transformation to the test data
-  test_PCA = test_image %*% P_pca
+  test_PCA = centered_test %*% P_pca
   
-  # Compute the distances between the test image and the previous data
-  aux = rbind(train_PCA, test_PCA)
-  distances = as.matrix(dist(aux),method=metric)
-  distance = distances[2, 1:1]
-  
-  # Predict the class based on the nearest neighbors
-  nearest_neighbors <- order(distance)[1:friends]
-  neighbor_class <- target[nearest_neighbors]
-  tb = table(neighbor_class)
-  
-  # Stablish if the person is in the dataset or not
-  if (min(distance) > threshold) {
-    # If the person in the image is not in the training dataset, set to 0
-    predicted_class = 0
-  } else {
-    # If the person in the image is in the training dataset it predicts which is the most similar
-    predicted_class = names(tb)[which.max(tb)]
-  }
+  predicted_class <- our_knn_single(train_pca, test_PCA, target, friends=3, threshold= 2800, metric='euclidean')
   
   return(predicted_class)
 }
 
 PCA_Classifier("anonym1.4.jpg")
+PCA_Classifier("9332898.15.jpg")
+PCA_Classifier("pspliu.1.jpg")
